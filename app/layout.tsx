41d1887dfecbe91821624c631c8316485ed3180c
Aditya Brahmankar <adityabrahmankar9@gmail.com>
2024-01-09 15:33:35 +0530

    og

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Nishyan | My Dukaan',
   description: 'Try Dukaan, the cutting-edge enterprise ecommerce platform. Seamlessly manage your online business and drive growth with our powerful tools.',
+  metadataBase: new URL("https://dukaan-payouts-dashboard-phi.vercel.app"),
   openGraph: {
     images: "opengraph-image.png",
   },
